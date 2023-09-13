6394e16e4d5028fb9b001355bf7562bea10ec57f
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-09-13 14:10:04 -0700

    Model amd class documentation added

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+# This is a base model for the Air BnB project
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
+    """Class for the Base Model"""
     def __init__(self):
+        """public instance id to have a unique id for each base model"""
         self.id = str(uuid.uuid4())
         self.create_at = datetime.now()
         self.update_at = datetime.now()
 
     def __str__(self):
+        """Public instance method"""
         return "[{}] ({}) {}".format(
                 self.__class__.__name__, self.id, self.__dict__)
 
