0e91e39a1da586bd674a2252cec33da967c36a98
Akos-1 <veronicagyasi@gmail.com>
2023-09-13 06:02:19 -0700

    removed provided test file

--- models/base_model.py
+++ models/base_model.py
@@ -6,8 +6,8 @@ from datetime import datetime
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.create_at = datetime.current()
-        self.update_at = datetime.current()
+        self.create_at = datetime.now()
+        self.update_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(
@@ -17,14 +17,14 @@ class BaseModel:
         """
         The update_at attribute with the current datetime is updated.
         """
-        self.update_at = datetime.current()
+        self.update_at = datetime.now()
 
     def to_dict(self):
         """
         A dictionary representation of the instance is returned.
         """
         instance_dict = self.__dict__.copy()
-        instance_dict['__class__'] = self.__class__.__name__
         instance_dict['create_at'] = self.create_at.isoformat()
         instance_dict['update_at'] = self.update_at.isoformat()
+        instance_dict['__class__'] = self.__class__.__name__  
         return instance_dict
