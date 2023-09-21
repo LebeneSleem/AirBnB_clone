51bba69b9564eae6bdeb1d351f398966c7209edc
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-09-21 11:24:23 -0700

    Test Advanced tasks

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-HBNBCommandd class definition
+HBNBCommand class definition
 Entry point of the command interpreter
 """
 import cmd
@@ -210,6 +210,184 @@ class HBNBCommand(cmd.Cmd):
             pass
 
         setattr(obj, attr_name, attr_value)
+        obj.save()
+
+    def do_class_show(self, arg):
+        """Print the string representation of an instance based on class and ID."""
+        args = arg.split()
+        if len(args) < 2:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        model_classes = {
+            "BaseModel": BaseModel,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review,
+            "User": User
+        }
+
+        if class_name not in model_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 3:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[2]
+        obj_key = class_name + "." + obj_id
+
+        obj_dict = storage.all()
+        if obj_key in obj_dict:
+            print(obj_dict[obj_key])
+        else:
+            print("** no instance found **")
+
+    def do_class_destroy(self, arg):
+        """Delete an instance based on class and ID."""
+        args = arg.split()
+        if len(args) < 2:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        model_classes = {
+            "BaseModel": BaseModel,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review,
+            "User": User
+        }
+
+        if class_name not in model_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 3:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[2]
+        obj_key = class_name + "." + obj_id
+
+        obj_dict = storage.all()
+        if obj_key in obj_dict:
+            del obj_dict[obj_key]
+            storage.save()
+        else:
+            print("** no instance found **")
+
+    def do_class_update(self, arg):
+        """Update an instance's attributes based on class, ID, attribute name, and value."""
+        args = arg.split()
+        if len(args) < 2:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        model_classes = {
+            "BaseModel": BaseModel,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review,
+            "User": User
+        }
+
+        if class_name not in model_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 3:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[2]
+        obj_key = class_name + "." + obj_id
+
+        obj_dict = storage.all()
+        if obj_key not in obj_dict:
+            print("** no instance found **")
+            return
+
+        if len(args) < 4:
+            print("** attribute name missing **")
+            return
+
+        if len(args) < 5:
+            print("** value missing **")
+            return
+
+        obj = obj_dict[obj_key]
+        attr_name = args[3]
+        attr_value = args[4]
+
+        try:
+            attr_value = eval(attr_value)
+        except (NameError, SyntaxError):
+            pass
+
+        setattr(obj, attr_name, attr_value)
+        obj.save()
+
+    def do_class_update_dict(self, arg):
+        """Update an instance's attributes based on class, ID, and a dictionary representation."""
+        args = arg.split()
+        if len(args) < 2:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        model_classes = {
+            "BaseModel": BaseModel,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review,
+            "User": User
+        }
+
+        if class_name not in model_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 3:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[2]
+        obj_key = class_name + "." + obj_id
+
+        obj_dict = storage.all()
+        if obj_key not in obj_dict:
+            print("** no instance found **")
+            return
+
+        if len(args) < 4:
+            print("** dictionary missing **")
+            return
+
+        # Convert the provided dictionary representation to a dictionary object
+        try:
+            dict_representation = eval("{" + " ".join(args[4:]) + "}")
+        except (NameError, SyntaxError, TypeError):
+            print("** invalid dictionary representation **")
+            return
+
+        obj = obj_dict[obj_key]
+
+        # Update the instance's attributes using the dictionary
+        for key, value in dict_representation.items():
+            setattr(obj, key, value)
+
         obj.save()
 
 
